210349eb1237628b6b32e623c4a88284a9aa2857
WEX 2 <work-experience2@outlook.com>
2026-07-14 14:21:48 +0100

    implemented getfact

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 ## setup-db: Set up the SQLite database.
 .PHONY: setup-db
 setup-db:
-	python3 database/migrations/migrate.py
+	python database/migrations/migrate.py
 
 ## db-shell: Open the SQLite database shell.
 .PHONY: db-shell
--- database/get_fact.py
+++ database/get_fact.py
@@ -11,10 +11,12 @@ def get_fact() -> Fact:
     with provider.cursor() as cur:
         # TODO: (Task P5.1) Use the category parameter to optionally filter the random fact query
         # TODO: (Task P0.1) Write SQL query to select a random fact from the database
-        cur.execute("") # SQL query goes here
+
+        cur.execute("SELECT * FROM facts;") # SQL query goes here
         # TODO: (Task P3.1) Add the likes and dislikes counts to the SQL query
         # TODO: (Task P4.2) Add the category column to the SQL query
         fact_data = fetch_fact_data(cur)
+
         if not fact_data:
             return Fact(id=None, fact="", category=None, likes=0, dislikes=0) # TODO: (Task P0.1) Create and return an empty Fact object if no result is found
 
@@ -23,7 +25,7 @@ def get_fact() -> Fact:
         # TODO: (Task P4.2) Add the category information
         return Fact(
             id=fact_data.id,
-            fact=None,     # TODO: (Task P0.1) Add parsed fact text
+            fact=fact_data.fact,     # TODO: (Task P0.1) Add parsed fact text
             likes=0,       # TODO: (Task P3.1) Add parsed likes count
             dislikes=0,    # TODO: (Task P3.1) Add parsed dislikes count
             category=None  # TODO: (Task P4.2) Add parsed category
