afc146b1cd7de3c9b8c8affe2a78555448077c6f
WEX 6 <work-experience6@outlook.com>
2026-07-15 15:14:57 +0100

    We have implemented the create fact function

--- database/create_fact.py
+++ database/create_fact.py
@@ -13,15 +13,15 @@ def create_fact(fact_text) -> Fact:
         # TODO: (Task P3.1) Add likes and dislikes counts to SQL query
         # TODO: (Task P4.3) Add category to SQL query
         print("Test for inserting into database")
-        cur.execute("INSERT INTO facts (fact) VALUES ('still working hopefully?');") # SQL query goes here
-        #fact_data = fetch_fact_data(cur)
+        cur.execute(f"INSERT INTO facts (fact, likes, dislikes, category) VALUES ('{fact_text}', 0, 0, 'animal');") # SQL query goes here
+        fact_data = fetch_fact_data(cur)
         provider.commit()
-        #if not fact_data:
-            #return Fact(id=None, fact="", category=None, likes=0, dislikes=0) # TODO: (Task P1.1) Create and return an empty Fact object if no result is found
+        if not fact_data:
+            return Fact(id=None, fact="", category=None, likes=0, dislikes=0) # TODO: (Task P1.1) Create and return an empty Fact object if no result is found
         
         return Fact(
-            id=0,
-            fact= None,   # TODO: (Task P1.1) Create and return a Fact object using the data from the database result
+            id=fact_data.id,
+            fact= fact_data.fact,   # TODO: (Task P1.1) Create and return a Fact object using the data from the database result
             likes=0,      # TODO: (Task P3.1) Add likes and dislikes counts to the Fact object
             dislikes=0,   
             category=None # TODO: (Task P4.3) Add category to returned fact
