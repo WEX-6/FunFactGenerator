b51d91facb365ded67d9657a5ea9c649a0dd8f89
WEX 6 <work-experience6@outlook.com>
2026-07-15 15:04:02 +0100

    We have implemented the create fact function

--- database/create_fact.py
+++ database/create_fact.py
@@ -4,22 +4,23 @@ from fact import Fact
 from .provider import SQLiteConnectionProvider
 from .helpers.database_helpers import fetch_fact_data
 
-# TODO: (Task P1.1) Add fact_text parameter
+# TODO: (Task P1.1) Add fact_text parameter DONE
 # TODO: (Task P4.3) Add category parameter
-def create_fact() -> Fact:
+def create_fact(fact_text) -> Fact:
     provider = SQLiteConnectionProvider()
     with provider.cursor() as cur:
         # TODO: (Task P1.1) Write SQL query to add new fact to the database
         # TODO: (Task P3.1) Add likes and dislikes counts to SQL query
         # TODO: (Task P4.3) Add category to SQL query
-        cur.execute("") # SQL query goes here
-        fact_data = fetch_fact_data(cur)
+        print("Test for inserting into database")
+        cur.execute("INSERT INTO facts (fact) VALUES ('still working hopefully?');") # SQL query goes here
+        #fact_data = fetch_fact_data(cur)
         provider.commit()
-        if not fact_data:
-            return Fact(id=None, fact="", category=None, likes=0, dislikes=0) # TODO: (Task P1.1) Create and return an empty Fact object if no result is found
+        #if not fact_data:
+            #return Fact(id=None, fact="", category=None, likes=0, dislikes=0) # TODO: (Task P1.1) Create and return an empty Fact object if no result is found
         
         return Fact(
-            id=fact_data.id,
+            id=0,
             fact= None,   # TODO: (Task P1.1) Create and return a Fact object using the data from the database result
             likes=0,      # TODO: (Task P3.1) Add likes and dislikes counts to the Fact object
             dislikes=0,   
